334c84d585a4789343cf06b6f675c0ad25356da4
tatsu9552 <srtt1210@gmail>
2024-07-02 15:54:40 +0900

    test

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
+import Footer from '@/component/footer/index'
+
 
 export default function Home() {
   return (
     <main>
       <h1>TOPページ</h1>
       <p>hello world</p>
+      <Footer/>
     </main>
   )
 }
---
+++ src/component/footer/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './footer.module.css'
+
+function index() {
+  return (
+    <div>
+        <h1 className={styles.title} >test</h1>
+    </div>
+  )
+}
+
+export default index
