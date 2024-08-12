b197253d77ff834369d96d80c604c6680c6341f1
Kipeo <kp225048@gmail.com>
2024-08-12 18:16:13 +0900

    footers修正byhasegawa

--- src/component/footer/index.jsx
+++ src/component/footer/index.jsx
@@ -1,30 +1,38 @@
-import React from 'react';
-import styles from './footer.module.css';
-import './index.css'; // グローバルスタイルをインポート
+import React from 'react'
+import styles from './footer.module.css'
+// import './index.css' // グローバルスタイルをインポート
 
 function Index() {
   return (
-    <div>
-      <header className={styles.header}>
+    <div className={styles.footerWrap}>
+      <div className={styles.header}>
         <h1 className={styles.title}>愛知工業大学 自然言語処理研究室</h1>
-      </header>
+      </div>
       <nav className={styles.nav}>
-        <a href="#" className={styles.link}>HOME</a>
-        <a href="#" className={styles.link}>ABOUT</a>
-        <a href="#" className={styles.link}>MEMBER</a>
-        <a href="#" className={styles.link}>ACHIEVEMENTS</a>
-        <a href="#" className={styles.link}>CONTACT</a>
+        <a href='#' className={styles.link}>
+          HOME
+        </a>
+        <a href='#' className={styles.link}>
+          ABOUT
+        </a>
+        <a href='#' className={styles.link}>
+          MEMBER
+        </a>
+        <a href='#' className={styles.link}>
+          ACHIEVEMENTS
+        </a>
+        <a href='#' className={styles.link}>
+          CONTACT
+        </a>
       </nav>
       <div className={styles.dots}>
         <span></span>
         <span></span>
         <span></span>
       </div>
-      <footer className={styles.footer}>
-        © 2024 AIT NLP TOKUHISA LAB.
-      </footer>
+      <footer className={styles.footer}>© 2024 AIT NLP TOKUHISA LAB.</footer>
     </div>
-  );
+  )
 }
 
-export default Index;
+export default Index
