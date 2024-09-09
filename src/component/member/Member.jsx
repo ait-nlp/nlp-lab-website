a1dab632bf113034dd5c3ea94a1ec11f9527ae36
ucbssnj <k22010kk@aitech.ac.jp>
2024-09-09 23:41:54 +0900

    img_input

--- src/component/member/Member.jsx
+++ src/component/member/Member.jsx
@@ -5,7 +5,7 @@ export default function Member() {
     {
       title: '教授',
       name: '教員名',
-      image: '/member_img/testicon.jpg',
+      image: '/member_img/professor.jpg',
       description: 'ここに教員情報を記入する。ここに教員情報を記入する。',
     },
   ];
@@ -13,20 +13,45 @@ export default function Member() {
   const students = [
     {
       name: 'ライオン',
-      image: '/member_img/testicon.jpg',
+      image: '/member_img/hida.jpg',
       description: 'がおーーーーーーーーーーーーーーー',
     },
     {
         name: 'ライオン',
-        image: '/member_img/testicon.jpg',
+        image: '/member_img/ichikawa.jpg',
         description: 'がおーーーーーーーーーーーーーーー',
       },
       {
         name: 'ライオン',
-        image: '/member_img/testicon.jpg',
+        image: '/member_img/kippei.png',
         description: 'がおーーーーーーーーーーーーーーー',
       },
-    // Add more student objects here
+      {
+        name: 'ライオン',
+        image: '/member_img/masui.jpg',
+        description: 'がおーーーーーーーーーーーーーーー',
+      },
+      {
+        name: 'ライオン',
+        image: '/member_img/Okajima.jpg',
+        description: 'がおーーーーーーーーーーーーーーー',
+      },
+      {
+        name: 'ライオン',
+        image: '/member_img/Otsuka.jpg',
+        description: 'がおーーーーーーーーーーーーーーー',
+      },
+      {
+        name: 'ライオン',
+        image: '/member_img/shibata.jpg',
+        description: 'がおーーーーーーーーーーーーーーー',
+      },
+      {
+        name: 'ライオン',
+        image: '/member_img/shimizu.jpg',
+        description: 'がおーーーーーーーーーーーーーーー',
+      },
+
   ];
 
   return (
