327bb10047071610bef24cb8bdf4958b31d9416d
ucbssnj <k22010kk@aitech.ac.jp>
2024-09-10 10:17:56 +0900

    syusei

--- src/component/member/student/Student.jsx
+++ src/component/member/student/Student.jsx
@@ -3,7 +3,7 @@ import styles from './staticMain.module.css';
 
 export default function Student({ student }) {
   return (
-    <div className={styles.student}>
+    <div className={styles.MemberItem}>
       <div className={styles.MemberImage}>
         <Image
           src={student.image}
